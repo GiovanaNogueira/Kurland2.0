91ac51ed1a33eda63d644464aab605394dccf365
GiovanaNogueira <gigimnogueira@gmail.com>
2024-06-19 16:11:58 -0300

    Initial Commit

---
+++ Kurland/KurlandApp.swift
@@ -0,0 +1,17 @@
+//
+//  KurlandApp.swift
+//  Kurland
+//
+//  Created by Giovana Nogueira on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct KurlandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
